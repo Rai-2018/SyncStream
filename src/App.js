4e1ea650332b215ad5a22432be7562c0cdb8fd5d
yunerc <yuner_cai@sfu.ca>
2021-07-15 23:21:21 -0700

    add video list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
 import {Player as VideoPlayer} from './components/video/video';
+import Upload from './components/upload/upload';
 
 import Login from './components/Auth/Login'
 import useToken from './components/App/useToken'
@@ -23,6 +24,7 @@ function App() {
           <Route path='/dashboard' exact component={Dashboard}/>
           <Route path='/preferences' exact component={Preferences}/>
           <Route path='/video' exact component={VideoPlayer}/>
+          <Route path='/upload' exact component={Upload}/>
         </Switch>
       </BrowserRouter>
     </div>
