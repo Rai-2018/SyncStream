8aaf1f8556e8f592cd10e46ea66fae35588aa308
lexik <lexike7419@gmail.com>
2021-08-02 21:36:47 -0700

    change the css of chatbox

--- src/components/chatbox/Chat.js
+++ src/components/chatbox/Chat.js
@@ -1,13 +1,21 @@
+
 import React, { useState, useEffect } from "react";
 import io from 'socket.io-client';
 import "./Chat.css";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import ForumIcon from '@material-ui/icons/Forum';
+
 
 const Message = ({ msg }) => {
   return (
       <div className="msg">
-          <span> { new Date(msg.date).toUTCString() } </span>
-          <span> { msg.userName } </span>
-          <span> { msg.content } </span>
+        {/* <span className="username">Lexi</span>
+        <span className="content">contentdddlksjflksfjlksdjflksjflksjfldddddddddddddd</span>
+        <span className="time">timeeeeeeeeee</span> */}
+        <span className="username"> { msg.userName } </span>
+        <span className="content"> { msg.content } </span>
+        <span className="time"> { new Date(msg.date).toUTCString() } </span> 
       </div>
   );
 };
@@ -30,11 +38,23 @@ const MessageBox = (props) => {
   };
 
   return (
+    <div className="TextField-without-border-radius">
       <form className="footer" onSubmit={ postMessage }>
-          <input type="text" className="input" placeholder="message"
+        <TextField 
+        id="outlined-basic" 
+        label="Type something" 
+        variant="outlined" 
+        fullWidth
+        size="small"
+        InputLabelProps={{style: {fontSize: 13}}}
+        value={ value } 
+        onChange={ e => setValue(e.target.value) }
+        />
+          {/* <input type="text" className="input" placeholder="message"
                  value={ value } onChange={ e => setValue(e.target.value) }
-          />
+          /> */}
       </form>
+      </div>
   );
 
 };
@@ -60,11 +80,19 @@ const Chat = (props) => {
   }, []);
 
   return (
-      <div>
+      <div className="chatapp"> 
+        <div className="chatbody">
+        <div className="chat_header">
+          <ForumIcon />
+        <h3>Chat Room</h3>
+        </div>
           <div id = "msgBox">
               { messages.map((msg, index) => <Message msg={msg} />) }
           </div>
+          <div className="chatfooter">
           <MessageBox socket={socket} room_id={props.room_id} user_id={props.user_id} />
+          </div>
+          </div>
       </div>
   );
 
